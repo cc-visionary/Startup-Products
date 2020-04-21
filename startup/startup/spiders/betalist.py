72f2b14b31c2fe4d109d6a756ae6a97c798e3581
Christopher Lim <lim.christopher.go@gmail.com>
2020-04-21 20:36:56 +0800

    Import scared data from betalist to betalist_final.xlsx

--- startup/startup/spiders/betalist.py
+++ startup/startup/spiders/betalist.py
@@ -1,14 +1,58 @@
 import scrapy
+import os
 import json
-# import pandas as pd
-# df = pd.read_excel('../betalist.xlsx')
-# total = len(df.index)
-# left = df[df['URL'] != df['URL']]
-# start_left = total - len(left)
-# print(left['Betalist URL'])
-# print(start_left)
+import xlsxwriter
+import pandas as pd
+df = pd.read_excel(os.getcwd() + '\\betalist_final.xlsx')
+print(len(df), df['Betalist URL'].nunique()) # proves whether or not every item scraped is unique (it is)
+# products = []
+# for data in df.iloc:
+#     products.append({
+#         'product': data['Product Name'],
+#         'description': data['Description'],
+#         'url': data['URL'],
+#         'href': data['Betalist URL'],
+#         'tag': data['Tag'],
+#         'votes': data['Votes']
+#     })
 
-datas = json.loads('../betalist.json')
+# with open(os.getcwd() + '\\startup\\startup\\betalist.json', 'r') as f:
+#     datas = json.load(f)
+#     for data in datas:
+#         products.append({
+#             'product': data['name'],
+#             'description': data['pitch'],
+#             'url': None,
+#             'href': 'https://www.betalist.com' + data['path'],
+#             'tag': [],
+#             'votes': data['upvote']
+#         })
+
+# workbook = xlsxwriter.Workbook('betalist_final.xlsx')
+# worksheet = workbook.add_worksheet()
+# worksheet.set_column('A:D', 25)
+# worksheet.set_column('E:E', 70)
+# worksheet.write(0, 0, 'Product Name')
+# worksheet.write(0, 1, 'Description')
+# worksheet.write(0, 2, 'URL')
+# worksheet.write(0, 3, 'Betalist URL')
+# worksheet.write(0, 4, 'Tag')
+# worksheet.write(0, 5, 'Votes')
+
+# row = 1
+# for product in products:
+#     worksheet.write(row, 0, product['product'])
+#     worksheet.write(row, 1, product['description'])
+#     worksheet.write(row, 2, None)
+#     worksheet.write(row, 3, product['href'])
+#     worksheet.write(row, 4, None)
+#     if(product['votes'] == None):
+#         worksheet.write(row, 5, 0)
+#     else:
+#         worksheet.write(row, 5, int(product['votes']))
+#     row += 1
+    
+# workbook.close()
 # for data in datas:
 #     print(data)
 
